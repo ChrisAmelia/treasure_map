1db3a69f135c5ddb3e77186da28fc17595ebd8b9
ChrisAmelia <chris@objectdev.io>
2022-05-07 22:53:50 +0200

    fix(TreasureMap): redefine width/height

--- src/main/java/com/treasuremap/app/model/TreasureMap.java
+++ src/main/java/com/treasuremap/app/model/TreasureMap.java
@@ -8,18 +8,40 @@ import lombok.Data;
 @Data
 public class TreasureMap {
 	/**
-	 * The width of the map.
+	 * The tiles of the map.
 	 *
-	 * @param width The width to set.
-	 * @return the width of the map.
+	 * @param tiles The tiles to set.
+	 * @return the tiles of the map.
 	 */
-	private int width;
+	private Tile[][] tiles;
 
 	/**
-	 * The height of the map.
+	 * Constructs a new object TreasureMap.
 	 *
-	 * @param height The height to set.
-	 * @return the height of the map.
+	 * @param rows the rows of the map.
+	 * @param cols the columns of the map.
 	 */
-	private int height;
+	public TreasureMap(int rows, int cols) {
+		tiles = new Tile[rows][cols];
+
+		for (int i = 0 ; i < rows ; i++) {
+			for (int j = 0 ; j < cols ; j++) {
+				tiles[i][j] = new Tile(i, j);
+			}
+		}
+	}
+
+	/**
+	 * Returns the width of the map.
+	 */
+	public int getWidth() {
+		return tiles.length;
+	}
+
+	/**
+	 * Returns the height of the map.
+	 */
+	public int getHeight() {
+		return tiles[0].length;
+	}
 }
--- src/test/java/com/treasuremap/app/model/TreasureMapTest.java
+++ src/test/java/com/treasuremap/app/model/TreasureMapTest.java
@@ -1,6 +1,7 @@
 package com.treasuremap.app.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
 /**
@@ -9,21 +10,26 @@ import org.junit.Test;
 public class TreasureMapTest {
 
 	/**
-	 * Testing getters and setters.
+	 * Check if the every tile of the map is properly initialized.
+	 * Check if the map has the proper dimensions.
 	 *
-	 * {@link TreasureMap#getWidth()}
-	 * {@link TreasureMap#getHeight()()}
-	 * {@link TreasureMap#setWidth(int)}
-	 * {@link TreasureMap#setHeight(int)}
+	 * Testing: constructor {@link #TreasureMap(int,int)}.
 	 */
 	@Test
-	public void modified_map_should_return_modified_values() {
-		TreasureMap map = new TreasureMap();
-		map.setWidth(7);
-		map.setHeight(9);
+	public void new_map_should_have_all_tiles_not_null_and_dimensions_not_null() {
+		TreasureMap map = new TreasureMap(5, 6);
+		Tile[][] tiles = map.getTiles();
 
-		assertEquals(7, map.getWidth());
-		assertEquals(9, map.getHeight());
+		for (int i = 0 ; i < tiles.length ; i++) {
+			for (int j = 0 ; j < tiles[i].length ; j++) {
+				if (tiles[i][j] == null) {
+					fail();
+				}
+			}
+		}
+
+		assertEquals(5, map.getWidth());
+		assertEquals(6, map.getHeight());
 	}
-	
+
 }
