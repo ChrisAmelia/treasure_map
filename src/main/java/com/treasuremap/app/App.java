b2d1df6766198deb063fe39afd68d7ce46d43590
ChrisAmelia <chris@objectdev.io>
2022-05-10 22:17:30 +0200

    chore: trivial example

--- src/main/java/com/treasuremap/app/App.java
+++ src/main/java/com/treasuremap/app/App.java
@@ -1,13 +1,32 @@
 package com.treasuremap.app;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import com.treasuremap.app.controller.Game;
+import com.treasuremap.app.model.Adventurer;
+import com.treasuremap.app.model.Orientation;
+import com.treasuremap.app.model.Tile;
+import com.treasuremap.app.model.TileType;
+import com.treasuremap.app.model.TreasureMap;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+    public static void main(String[] args) {
+		Adventurer adventurer = new Adventurer();
+		adventurer.setOrientation(Orientation.EAST);
+		adventurer.setPath("AADADAGA");
+
+		TreasureMap map = new TreasureMap(3, 3);
+		map.addAdventurer(adventurer, 0, 0);
+
+		Game game = new Game();
+		game.setMap(map);
+		game.setAdventurers(Stream.of(adventurer).collect(Collectors.toList()));
+		game.executeAdventurerPath(adventurer);
+	}
+
 }
