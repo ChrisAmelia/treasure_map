b11ffe5681ae483ea91f10f5b4f31f1c8ba22a04
ChrisAmelia <chris@objectdev.io>
2022-05-13 23:06:24 +0200

    style(Game): state of current game

--- src/main/java/com/treasuremap/app/controller/Game.java
+++ src/main/java/com/treasuremap/app/controller/Game.java
@@ -3,6 +3,7 @@ package com.treasuremap.app.controller;
 import java.util.List;
 import com.treasuremap.app.model.Adventurer;
 import com.treasuremap.app.model.Orientation;
+import com.treasuremap.app.model.Tile;
 import com.treasuremap.app.model.TreasureMap;
 import lombok.Getter;
 import lombok.Setter;
@@ -56,5 +57,107 @@ public class Game {
 				System.err.println("Error, symbol '" + c + "' unknown.");
 			}
 		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+
+		builder.append(map.toString());
+		builder.append("\n\n");
+		builder.append(adventurersInfo());
+		builder.append("\n");
+		builder.append(mountainsInfo());
+		builder.append("\n");
+		builder.append(treasuresInfo());
+
+		return builder.toString();
+	}
+
+	/**
+	 * Returns a string containing the current state of the adventurers.
+	 *
+	 * @return a string containing the current state of the adventurers.
+	 */
+	private String adventurersInfo() {
+		StringBuilder builder = new StringBuilder("[ Adventurers ]\n");
+
+		if (adventurers.isEmpty()) {
+			builder.append("No adventurers");
+
+			return builder.toString();
+		}
+
+		for (Adventurer adventurer : adventurers) {
+			builder.append(adventurer.getName());
+			builder.append(": ");
+			builder.append("(" + (adventurer.getX() + 1) + "," + (adventurer.getY() + 1) + "), ");
+			builder.append("treasures=" + adventurer.getTreasures());
+			builder.append("\n");
+		}
+
+		return builder.toString();
+	}
+
+	/**
+	 * Returns a string containing the locations of mountains.
+	 *
+	 * @return a string containing the locations of mountains.
+	 */
+	private String mountainsInfo() {
+		StringBuilder builder = new StringBuilder("[ Mountains ]\n");
+		boolean hasMountain = false;
+
+		Tile[][] tiles = map.getTiles();
+
+		for (int i = 0 ; i < tiles.length ; i++) {
+			for (int j = 0 ; j < tiles[i].length ; j++) {
+				Tile tile = tiles[i][j];
+
+				if (tile.isMountain()) {
+					hasMountain = true;
+
+					builder.append("(" + (tile.getX() + 1) + "," + (tile.getY() + 1) + ")");
+					builder.append("\n");
+				}
+			}
+		}
+
+		if (!hasMountain) {
+			builder.append("No mountains\n");
+		}
+
+		return builder.toString();
+	}
+
+	/**
+	 * Returns a string containing the locations of treasures.
+	 *
+	 * @return a string containing the locations of treasures.
+	 */
+	private String treasuresInfo() {
+		StringBuilder builder = new StringBuilder("[ Treasures ]\n");
+		boolean hasTreasures = false;
+
+		Tile[][] tiles = map.getTiles();
+
+		for (int i = 0 ; i < tiles.length ; i++) {
+			for (int j = 0 ; j < tiles[i].length ; j++) {
+				Tile tile = tiles[i][j];
+
+				if (tile.hasTreasures()) {
+					hasTreasures = true;
+
+					builder.append("(" + (tile.getX() + 1) + "," + (tile.getY() + 1) + "), quantity=" + tile.getTreasures());
+					builder.append("\n");
+				}
+			}
+		}
+
+		if (!hasTreasures) {
+			builder.append("No treasures\n");
+		}
+
+		return builder.toString();
 	}
 }
